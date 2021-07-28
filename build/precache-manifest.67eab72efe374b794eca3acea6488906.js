969ea565313a687ca0683fb41532499d8c541b80
Ambica <ab185462@ncr.com>
2021-07-29 00:34:03 +0530

    LoginPOS

---
+++ build/precache-manifest.67eab72efe374b794eca3acea6488906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c16d4818bbd8022198ac2d250820cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4efaba4da2781fe10ed7",
+    "url": "/static/css/main.184e7dff.chunk.css"
+  },
+  {
+    "revision": "3e2508b349c57775725b",
+    "url": "/static/js/2.bf527581.chunk.js"
+  },
+  {
+    "revision": "2d21d72fd4a5b94d8a3d931708b3a215",
+    "url": "/static/js/2.bf527581.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4efaba4da2781fe10ed7",
+    "url": "/static/js/main.f716b5d0.chunk.js"
+  },
+  {
+    "revision": "0a2196dc6f656b36fa82",
+    "url": "/static/js/runtime-main.007f975e.js"
+  }
+]);
